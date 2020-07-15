fd5077608434446b1b1ebf95396816e3c7d99206
Denise Henderson <denise@henderson3.us>
2020-07-15 23:23:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,10 +15,5 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  new_array=[]
-  new_array << array[0]
-  new_array << array[2]
-  new_array << array[1]
-  i=3
-  new_array << array[3]
+  array[1], array[2] = array[2], array[1]
 end
