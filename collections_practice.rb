0c466893ef6e92b33090bce2505bd82eb1107c7c
Denise Henderson <denise@henderson3.us>
2020-07-15 23:16:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |b,a|
+    a<=>b
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |x, y|
+   x.length <=> y.length
+  end
+end
+
+def swap_elements(array)
+  new_array=[]
+  new_array << array[0]
+  new_array << array[2]
+  new_array << array[1]
+  i=3
+  new_array << array[3]
+end
